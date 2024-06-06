08daa84e37dd0e3d579d88604cfe6c02eeec2952
Vinicius-de-souza-dev <126343704+Vinicius-de-souza-dev@users.noreply.github.com>
2024-06-06 18:23:00 -0300

    Incluindo a API.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,4 +69,6 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     implementation("androidx.navigation:navigation-compose:2.7.7")
+
+    implementation ("com.pix4d:pix4dlib:3.0.3")
 }
